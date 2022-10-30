2365f5cb8bba39d11460353156de25d3305c27f1
Rajai kumar <39953045+Rajaikumar-iOSDev@users.noreply.github.com>
2022-10-30 10:13:52 +0530

    Initial Commit

---
+++ BookKeeper/BooksTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  BooksTableViewController.swift
+//  BookKeeper
+//
+//  Created by Rajai kumar on 24/10/22.
+//
+
+import UIKit
+
+class BooksTableViewController: UITableViewController {
+
+    // MARK: - Properties
+    var book: BookKeeper?
+    var bookKeeperService: BookKeeperService?
+    @IBOutlet weak var bookNameTextField: UITextField!
+    @IBOutlet weak var bookEditionTextField: UITextField!
+    @IBOutlet weak var bookVolumeTextField: UITextField!
+    @IBOutlet weak var bookPriceTextField: UITextField!
+    @IBOutlet weak var bookReleaseYearTextField: UITextField!
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // disables cell highlighting
+      tableView.allowsSelection = false
+
+      let formatter = DateFormatter()
+      formatter.dateStyle = .short
+
+      // Display values of selected report
+      if let book = book {
+        bookNameTextField.text = book.bookName
+          bookEditionTextField.text = String(describing: book.edition)
+          bookVolumeTextField.text = String(describing:book.volume)
+          bookPriceTextField.text = String(describing:book.price)
+          bookReleaseYearTextField.text = String(describing:book.releaseYear)
+      }
+    }
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    @IBAction func saveAction(_ sender: Any) {
+        let bookName = bookNameTextField.text ?? ""
+        let bookEdition = bookEditionTextField.text ?? ""
+        let bookVolume = Int32(bookVolumeTextField.text ?? "") ?? 0
+        let bookPrice = Int32(bookPriceTextField.text ?? "") ?? 0
+        let bookReleaseYear = Int32(bookReleaseYearTextField.text ?? "") ?? 0
+
+        if let book = book {
+          book.bookName = bookName
+          book.edition = bookEdition
+          book.volume = bookVolume
+          book.price = bookPrice
+          book.releaseYear = bookReleaseYear
+          bookKeeperService?.update(book)
+          dismiss(animated: true, completion: nil)
+        }else {
+            bookKeeperService?.add(bookName, edition: bookEdition, volume: bookVolume, price: bookPrice, releaseYear: bookReleaseYear)
+            dismiss(animated: true, completion: nil)
+          }
+    }
+    
+}
